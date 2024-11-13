21d9536ea02db5b3a8f7813b0d27c764dc1d82fe
elias <elias@Laptop-Elias>
2024-11-13 19:53:51 +0100

    update

---
+++ Programmierung1/src/Übungsaufgaben/Dice.java
@@ -0,0 +1,33 @@
+package Übungsaufgaben;
+
+import java.util.Random;
+
+public class Dice {
+	
+	Random myRandom = new Random();
+	private final int id;
+	private int value;
+	
+	public Dice(int id) {
+		this.id = id;
+	}
+	public int getId() {
+		System.out.print("Die ID des Würfels lautet: "); 
+		return this.id;
+		
+	}
+	public int getValue() {
+		return this.value;
+	}
+	public void rollTheDice(int id) {
+		System.out.println("ID - Würfelwert");
+		for (int i = 0; i <= 5; i++) {
+			this.value = myRandom.nextInt(1, 7);
+			System.out.println(this.id + " - " + this.value);
+		}
+	}
+	
+	
+	
+	
+}
---
+++ Programmierung1/src/Übungsaufgaben/Dice_main.java
@@ -0,0 +1,13 @@
+package Übungsaufgaben;
+
+import Übungsaufgaben.Dice;
+
+public class Dice_main {
+
+	public static void main(String[] args) {
+		Dice Würfel = new Dice(1);
+		Würfel.rollTheDice(1);
+		Würfel.getId();
+	
+	}
+}
