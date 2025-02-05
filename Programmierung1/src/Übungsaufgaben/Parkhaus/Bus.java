5bfb3f9346f8a035a63360a0cb1962a09e4a29e6
lwesce13 <lwesce13@LWENB3609.lwe.liebherr.i>
2025-02-05 09:01:59 +0100

    update

--- Programmierung1/src/Übungsaufgaben/Parkhaus/Bus.java
+++ Programmierung1/src/Übungsaufgaben/Parkhaus/Bus.java
@@ -1,7 +1,7 @@
 package Übungsaufgaben.Parkhaus;
 
 public class Bus extends Vehicle {
-	public Bus(java.lang.String make, java.lang.String model, double lengthInM, double heightInM) {
+	public Bus(String make, String model, double lengthInM, double heightInM) {
 		super(make, model, lengthInM, heightInM);
 	}
 }
