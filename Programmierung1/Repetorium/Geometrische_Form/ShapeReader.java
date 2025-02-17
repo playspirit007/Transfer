f5d097c6ce5683e4306dbb3a333c3fcfbff6a7dc
lwesce13 <lwesce13@LWENB3609.lwe.liebherr.i>
2025-02-17 14:18:48 +0100

    Update

--- Programmierung1/Repetorium/Geometrische_Form/ShapeReader.java
+++ Programmierung1/Repetorium/Geometrische_Form/ShapeReader.java
@@ -2,13 +2,25 @@ package Geometrische_Form;
 
 import java.awt.Shape;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ShapeReader {
 
   private ArrayList<Shape> shapes;
 
-  public ShapeReader(File file) {
+  public ShapeReader() throws FileNotFoundException {
     shapes = new ArrayList<>();
+    File file = new File("Repetorium/Geometrische_Form/Forms.txt");
+    // /Programmierung/Repetorium/Geometrische_Form/Forms.txt
+
+    Scanner myScanner = new Scanner(file);
+
+    int i = 0;
+    while (myScanner.hasNextLine()) {
+      String buffer = myScanner.nextLine();
+      String[] input = buffer.split(";");
+    }
   }
 }
--- Programmierung1/Repetorium/Geometrische_Form/Square.java
+++ Programmierung1/Repetorium/Geometrische_Form/Square.java
@@ -1,8 +1,20 @@
 package Geometrische_Form;
 
-public class Square extends Rectangle {
+public class Square extends Shape {
+
+  private final double aInCm;
 
   public Square(double aInCm) {
-    super(aInCm);
+    this.aInCm = aInCm;
+  }
+
+  @Override
+  public double getAreaInCm2() {
+    return aInCm * aInCm;
+  }
+
+  @Override
+  public double getCircumferenceInCm() {
+    return 4 * aInCm;
   }
 }
