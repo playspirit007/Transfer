59012a65dead086729711eea275290824fa20be8
BUERO-Elias <BUERO-Elias@BUERO-Elias.fritz.box>
2025-05-10 22:36:46 +0200

    update

---
+++ Programmierung1/Programmierung_II_Fortgeschrittene_Programmierung/Videosammlung/FileNotFoundException.java
@@ -0,0 +1,11 @@
+package Videosammlung;
+
+public class FileNotFoundException extends Exception{
+
+	private static final long serialVersionUID = 1L;
+	
+	public FileNotFoundException (String text) {
+		super(text);
+	}
+
+}
--- Programmierung1/Programmierung_II_Fortgeschrittene_Programmierung/Videosammlung/VideoCollection.java
+++ Programmierung1/Programmierung_II_Fortgeschrittene_Programmierung/Videosammlung/VideoCollection.java
@@ -1,6 +1,10 @@
 package Videosammlung;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Optional;
+import java.util.Scanner;
 
 public record VideoCollection(ArrayList<Video> videos) {
 	
@@ -8,7 +12,20 @@ public record VideoCollection(ArrayList<Video> videos) {
 		videos.add(video);
 	}
 	
-	public void importVideos(File file) {
-		
+	public void importVideos(File file) throws FileNotFoundException {
+		try (Scanner myScanner = new Scanner(file)) {
+			if (!myScanner.hasNextLine()) {
+				throw new FileNotFoundException("Datei ist leer!");
+			}
+		}
+	}
+	
+	public Optional<Video> getVideoByTitle(String title) {
+	    for (Video v : videos) {
+	        if (v.getMovie().title().equals(title)) {
+	            return Optional.of(v);
+	        }
+	    }
+	    return Optional.empty();
 	}
 }
