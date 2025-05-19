dd49165ae724fc152cddcf97525bf685c2c338cc
elias <elias@192.168.0.143>
2025-05-19 23:38:50 +0200

    update

--- Programmierung1/Programmierung_II_Fortgeschrittene_Programmierung/JavaStreamAPI01/ConsoleQueries.java
+++ Programmierung1/Programmierung_II_Fortgeschrittene_Programmierung/JavaStreamAPI01/ConsoleQueries.java
@@ -3,6 +3,8 @@ package JavaStreamAPI01;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
+import java.util.OptionalDouble;
 import java.util.stream.Collectors;
 
 public record ConsoleQueries(ArrayList<Console> consoles) {
@@ -29,4 +31,43 @@ public record ConsoleQueries(ArrayList<Console> consoles) {
 		return consoles.stream().allMatch(c -> c.soldUnitsInMillions() > 50_000_0000);
 	}
 
+	public long getNumberOfConsolesFromNintendo() {
+		long rück = 0;
+		for (Console c : consoles) {
+			if (c.maker() == Maker.NINTENDO) {
+				rück += c.soldUnitsInMillions();
+			}
+		}
+		return rück;
+	}
+
+	public List<String> getSoldUnitsInMillionsPerYearFromAllOutdatedConsoles() {
+		List<String> rück = new ArrayList<>();
+		for (Console c : consoles) {
+			if (c.lifespan() == -1) {
+				System.out.println(c.toString());
+				rück.add(c.title());
+			}
+		}
+		return rück;
+	}
+
+	public Optional<Double> getAverageSoldUnitsInMillionsPerYearFromAllOutdatedConsoles() {
+		double average = 0;
+		int counter = 0;
+		for (Console c : consoles) {
+			if (c.lifespan() == -1) {
+				average += c.soldUnitsInMillions();
+				counter++;
+			}
+		}
+		if (counter != 0) {
+			average = average / counter;
+		}
+		return Optional.ofNullable(average);
+	}
+
+	public OptionalDouble getAverageSoldUnitsInMillionsPerYearFromAllOutdatedConsoles1() {
+		return consoles.stream().filter(c -> c.lifespan() == -1).mapToDouble(Console::soldUnitsInMillions).average();
+	}
 }
