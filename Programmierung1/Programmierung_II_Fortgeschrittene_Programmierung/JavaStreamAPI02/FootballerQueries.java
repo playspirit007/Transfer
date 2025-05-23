f25127a57eae3977d17f18473adbcbce33b50b0c
BUERO-Elias <BUERO-Elias@BUERO-Elias.fritz.box>
2025-05-23 23:29:27 +0200

    update

--- Programmierung1/Programmierung_II_Fortgeschrittene_Programmierung/JavaStreamAPI02/FootballerQueries.java
+++ Programmierung1/Programmierung_II_Fortgeschrittene_Programmierung/JavaStreamAPI02/FootballerQueries.java
@@ -1,7 +1,12 @@
 package JavaStreamAPI02;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.OptionalDouble;
+import java.util.stream.Collectors;
 
 public record FootballerQueries(List<Footballer> footballers) {
 	
@@ -11,7 +16,7 @@ public record FootballerQueries(List<Footballer> footballers) {
 	
 	public Optional<String> getNameOfVfLWolfsburgFootballerWithMostPlayedGames() {
 		String name = null;
-		Footballer fmost = null;
+		Footballer fmost = footballers.get(0);
 		for (Footballer f : footballers) {
 			if (f.footballClub().name().contains("VfLWolfsburg")) {
 				if (fmost.numberOfGames() < f.numberOfGames()) {
@@ -23,7 +28,32 @@ public record FootballerQueries(List<Footballer> footballers) {
 	}
 	
 	public List<FootballClub> getAllFootballClubs() {
-		List<FootballClub> clubs = new List<>();
-		
+		List<FootballClub> clubs = new ArrayList<>();
+		for (Footballer f : footballers) {
+			clubs.add(f.footballClub());
+		}
+		return clubs;
+	}
+	
+	public boolean isFootballerWithSizeInCmLT170AndNumbreOfGoalsBT0() {
+		boolean back = false;
+		for (Footballer f : footballers) {
+			if (f.numberOfGoals() >= 1 && f.sizeInCm() < 170) {
+				back = true;
+			}
+		}
+		return back;
+	}
+	
+	public Map<Integer, List<Footballer>> getAllFootballersByBirthyear() {
+		Map<Integer, List<Footballer>> back = new HashMap<>();
+		for (Footballer f : footballers) {
+			back.put(f.birthdate().getYear(), new ArrayList<>()).add(f);
+		}
+		return back;
+	}
+	
+	public Optional<OptionalDouble> getAverageNumberOfPointsFromAllBundesligaFootballClubs() {
+		return Optional.ofNullable(footballers.stream().mapToDouble(footballer -> footballer.footballClub().pointsInAllTimeTable()).average());
 	}
 }
