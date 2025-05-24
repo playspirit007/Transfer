89a0b82bc6cf13512582b96831d5faf14e14a984
BUERO-Elias <BUERO-Elias@BUERO-Elias.fritz.box>
2025-05-24 15:25:40 +0200

    update

--- Programmierung1/Programmierung_II_Fortgeschrittene_Programmierung/Spielfiguren/CharacterQueries.java
+++ Programmierung1/Programmierung_II_Fortgeschrittene_Programmierung/Spielfiguren/CharacterQueries.java
@@ -2,9 +2,12 @@ package Spielfiguren;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.OptionalDouble;
+import java.util.stream.Collectors;
 
 public record CharacterQueries(List<Character> characters) {
 
@@ -33,6 +36,12 @@ public record CharacterQueries(List<Character> characters) {
 	public List<String> getUniqueCharacterNames() {
 		List<String> back = new ArrayList<>();
 		back = characters.stream().map(c -> c.name()).distinct().sorted((n1, n2) -> n2.compareTo(n1)).toList();
+		return back;
+	}
+	
+	public Map<Race, List<Character>> getCharactersByRace() {
+		Map<Race, List<Character>> back = new HashMap<>();
+		back = characters.stream().collect(Collectors.groupingBy(c -> c.race()));
 		return back;
 	}
 }
